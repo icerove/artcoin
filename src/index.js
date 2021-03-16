458cc18f08ccf0bdd82d8137d349cc5c1b43ed1b
Yifang Ma <yifangma93@gmail.com>
2021-03-15 21:10:49 -0700

    set up finish

--- src/index.js
+++ src/index.js
@@ -3,9 +3,57 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import * as nearAPI from "near-api-js";
+import getConfig from "./config";
+
+// Initializing contract
+async function initContract() {
+  const nearConfig = getConfig(process.env.NODE_ENV || 'testnet');
+
+  // Initializing connection to the NEAR TestNet
+  const near = await nearAPI.connect({
+    deps: {
+      keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore()
+    },
+    ...nearConfig
+  });
+
+  // Needed to access wallet
+  const walletConnection = new nearAPI.WalletConnection(near);
+
+  // Load in account data
+  let currentUser;
+  if(walletConnection.getAccountId()) {
+    currentUser = {
+      accountId: walletConnection.getAccountId(),
+      balance: (await walletConnection.account().state()).amount
+    };
+  }
+
+  // Initializing our contract APIs by contract name and configuration
+  const contract = await new nearAPI.Contract(walletConnection.account(), nearConfig.contractName, {
+    // View methods are read-only – they don't modify the state, but usually return some value
+    viewMethods: ['get_total_supply', 'get_total_balance', 'get_unstaked_balance', 'get_staked_balance', 'get_price'],
+    // Change methods can modify the state, but you don't receive the returned value when called
+    changeMethods: ['stake_and_mint', 'burn_to_unstake'],
+    // Sender is the account ID to initialize transactions.
+    // getAccountId() will return empty string if user is still unauthorized
+    sender: walletConnection.getAccountId()
+  });
+
+  return { contract, currentUser, nearConfig, walletConnection };
+}
+
+window.nearInitPromise = initContract()
+  .then(({ contract, currentUser, nearConfig, walletConnection }) => {
+    ReactDOM.render(
+      <App
+        contract={contract}
+        currentUser={currentUser}
+        nearConfig={nearConfig}
+        wallet={walletConnection}
+      />,
+      document.getElementById('root')
+    );
+  });
+
