f8656402b7ecd16176012ed67df44aa47d91499e
Yifang Ma <yifangma93@gmail.com>
2021-04-03 21:28:43 -0700

    change color

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -3,7 +3,10 @@ import { Table } from 'react-bootstrap'
 import { formatNearAmount } from "near-api-js/lib/utils/format";
 import tokenIcon from './tokenIcon'
 
+import { coinList } from './Dash'
+
 const Account = ({contract, accountId, ausdContract}) => {
+    const _coinList = coinList.filter((coin) => coin !== 'art')
 
     const [assetB, setAssetB] = useState({'aNEAR':'0', 'aBTC':'0', 'aGOLD':'0', 'aSPY':'0', 'aEUR':'0'})
 
@@ -20,16 +23,26 @@ const Account = ({contract, accountId, ausdContract}) => {
 
     useEffect(() => {
         async function loadBalance() {
-            let aBTC = await loadAssetBalance('aBTC')
-            let aNEAR = await loadAssetBalance('aNEAR')
-            let aGOLD = await loadAssetBalance('aGOLD')
-            let aSPY = await loadAssetBalance('aSPY')
-            let aEUR =await loadAssetBalance('aEUR')
-            setAssetB({aBTC, aNEAR, aGOLD, aSPY, aEUR})
+            let asset = {aNEAR: null, aBTC: null, aGOLD: null, aSPY: null, aEUR: null}
+
+            for(const p in asset){
+                let ass = await loadAssetBalance(p)
+                console.log(ass)
+                asset[p] = ass
+            }
+
+            setAssetB(asset)
         }
         loadAUSDBalance()
         loadBalance()
     }, [])
+
+    const Tr = ({asset, index}) => 
+    <tr>
+        <th>{index + 1}</th>
+        <td><img src={tokenIcon[asset]} alt='icon' className="icon"/>{asset}</td>
+        <td>{formatNearAmount(assetB[asset], 5)}</td>
+    </tr>
     
     return <div className="trade-card">
     <Table striped bordered hover>
@@ -46,31 +59,7 @@ const Account = ({contract, accountId, ausdContract}) => {
                 <td><img src={tokenIcon.aUSD} alt='icon' className="icon"/>aUSD</td>
                 <td>{formatNearAmount(aUSDBalnce,5)}</td>
             </tr>
-            <tr>
-                <th>1</th>
-                <td><img src={tokenIcon.aBTC} alt='icon' className="icon"/>aBTC</td>
-                <td>{formatNearAmount(assetB['aBTC'], 5)}</td>
-            </tr>
-            <tr>
-                <th>2</th>
-                <td><img src={tokenIcon.aNEAR} alt='icon' className="icon"/>aNEAR</td>
-                <td>{assetB['aNEAR']}</td>
-            </tr>
-            <tr>
-                <th>3</th>
-                <td><img src={tokenIcon.aGOLD} alt='icon' className="icon"/>aGOLD</td>
-                <td>{formatNearAmount(assetB['aGOLD'],5)}</td>
-            </tr>            
-            <tr>
-                <th>4</th>
-                <td><img src={tokenIcon.aEUR} alt='icon' className="icon"/>aEUR</td>
-                <td>{formatNearAmount(assetB['aEUR'],5)}</td>
-            </tr>
-            <tr>
-                <th>5</th>
-                <td><img src={tokenIcon.aSPY} alt='icon' className="icon"/>aSPY</td>
-                <td>{formatNearAmount(assetB['aSPY'],5)}</td>
-            </tr>
+            {_coinList.map((coin,index) => <Tr asset={coin} index={index} />)}
         </tbody>
     </Table>
     </div>
