3fee21dfa5df9a871f2f63164ee377e7699deb89
Yifang Ma <yifangma93@gmail.com>
2021-08-21 20:04:40 +0800

    udpate error

--- src/components/utils/Alerts.js
+++ src/components/utils/Alerts.js
@@ -1,20 +1,24 @@
-import React from 'react'
-import {Alert} from 'react-bootstrap'
+import React from "react";
+import { Alert } from "react-bootstrap";
 
-const AlertBanner = ({error, setAlert, setError}) => (
-    <Alert  
-        variant={'danger'} 
-        onClose={() => {
-            setAlert(false)
-            setError('')
-        }} 
-        dismissible
-    >
-        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-        <p>
-            Error: {error}. Try again later
-        </p>
-    </Alert>
-)
+const AlertBanner = ({ error, setAlert, setError }) => (
+  <Alert
+    variant={"danger"}
+    onClose={() => {
+      setAlert(false);
+      setError("");
+    }}
+    dismissible
+  >
+    <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+    <p>Error: {error}. Try again later.</p>
+    <p>
+      {" "}
+      For more information, please have a look at latest transaction at
+      https://explorer.testnet.near.org/accounts/art.artcoin.testnet or
+      https://explorer.testnet.near.org/accounts/ausd.artcoin.testnet
+    </p>
+  </Alert>
+);
 
-export default AlertBanner
+export default AlertBanner;
